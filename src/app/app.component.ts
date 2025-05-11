6da014f4b7883231f7a9013bddc64af33318cf5e
Giuseppe Mandriani <giuseppe.mandriani@gmail.com>
2025-05-11 11:35:07 +0200

    ..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   public title: string = 'angular-course';
   public dynamicValue: number = 0;
+  public imgLogoConfig = {
+    src: "https://miro.medium.com/v2/resize:fit:1400/1*WWRm1tIQ5MinLE0rUTo62w.png",
+    alt: "Logo Angular",
+    width: 200,
+  }
 }
