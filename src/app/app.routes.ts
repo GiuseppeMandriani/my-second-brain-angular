ed74ba120c239277172bcd643834732a07250e45
Giuseppe Mandriani <giuseppe.mandriani@gmail.com>
2025-05-13 15:43:51 +0200

    feat: added home page and landing page for test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './features/home-page/home-page/home-page.component';
+import { LandingTestComponent } from './features/landing-test/landing-test/landing-test.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomePageComponent },
+    { path: 'landing-test', component: LandingTestComponent },
+];
