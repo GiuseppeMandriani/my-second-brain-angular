f49e960f66d4ce9b9afc79d04244409aacf27d1f
Giuseppe Mandriani <giuseppe.mandriani@gmail.com>
2025-05-19 17:33:04 +0200

    feat: added lazy-loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,12 @@
 import { Routes } from '@angular/router';
-import { HomePageComponent } from './features/home-page/home-page/home-page.component';
-import { LandingTestComponent } from './features/landing-test/landing-test/landing-test.component';
-import { Demo1Component } from './features/demo1/demo1.component';
-import { Demo2Component } from './features/demo2/demo2.component';
 
 export const routes: Routes = [
     // { path: '', component: HomePageComponent },
     // { path: 'landing-test', component: LandingTestComponent },
 
-    { path: 'homepage', component: HomePageComponent },
-    { path: 'landing-test', component: LandingTestComponent },
-    { path: 'demo1', component: Demo1Component },
-    { path: 'demo2', component: Demo2Component },
+    { path: 'homepage', loadComponent: () => import('./features/home-page/home-page/home-page.component').then(c => c.HomePageComponent) },
+    { path: 'landing-test', loadComponent: () => import('./features//landing-test/landing-test/landing-test.component').then(c => c.LandingTestComponent) },   
+    { path: 'demo1', loadComponent: () => import('./features/demo1/demo1.component').then(c => c.Demo1Component) },    
+    { path: 'demo2', loadComponent: () => import('./features/demo2/demo2.component').then(c => c.Demo2Component) },
     { path: '', redirectTo: 'homepage', pathMatch: 'full'}
 ];
