0050414c2c8390322f981daedcddb3dd5d6aece6
Giuseppe Mandriani <giuseppe.mandriani@gmail.com>
2025-05-23 15:51:32 +0200

    added server render

--- src/app/core/api/base/constants/base-api-config.token.ts
+++ src/app/core/api/base/constants/base-api-config.token.ts
@@ -5,8 +5,11 @@ export const API_URLS = new InjectionToken<Record<string, string>>('API_URLS', {
   factory: () => ({
     discogs: 'https://api.discogs.com/',
     users: 'https://jsonplaceholder.typicode.com/users',
-    localTodo: 'http://localhost:3000',
-    localProducts: 'http://localhost:3000',
-    localConfigs: 'http://localhost:3000',
+    // localTodo: 'http://localhost:3000',
+    // localProducts: 'http://localhost:3000',
+    // localConfigs: 'http://localhost:3000',
+    localTodo: 'https://json-server-api-v7rs.onrender.com', //SERVER HOST
+    localProducts: 'https://json-server-api-v7rs.onrender.com', //SERVER HOST
+    localConfigs: 'https://json-server-api-v7rs.onrender.com', //SERVER HOST
   }),
 });
