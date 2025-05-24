f5e093b9c99b923b459bfafbd640af1bf7ebea60
Giuseppe Mandriani <giuseppe.mandriani@gmail.com>
2025-05-24 10:45:33 +0200

    auth config wip

--- src/app/core/components/navbar-2/navbar-2.component.ts
+++ src/app/core/components/navbar-2/navbar-2.component.ts
@@ -1,8 +1,10 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { CartService } from '../../../services/cart/cart.service';
 import { RouterLink } from '@angular/router';
 import { SettingsService } from '../../../services/settings/settings.service';
 import { AuthService } from '@auth0/auth0-angular';
+import { AsyncPipe, DOCUMENT } from '@angular/common';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-navbar-2',
@@ -14,7 +16,14 @@ import { AuthService } from '@auth0/auth0-angular';
 export class Navbar2Component {
   cartService = inject(CartService);
   settingsService = inject(SettingsService);
-  auth = inject(AuthService)
+  auth = inject(AuthService);
+  document = inject(DOCUMENT);
+  authSignal = toSignal(this.auth.isAuthenticated$);
+
+  user = toSignal(inject(AuthService).user$);
+  userImage = computed<string>(() => {
+    return this.user()?.picture || 'https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp'
+  })
 
 
 }
--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { Component, computed, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [AsyncPipe, JsonPipe],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export default class ProfileComponent {
+
+  // user$ = inject(AuthService).user$;
+  user = toSignal(inject(AuthService).user$);
+  yourData = computed(() => {
+    return `${this.user()?.name} (${this.user()?.email})`
+  })
 
 }
